c339d828c5a60b382e761cfca2e2154ea1d7274b
NakedSam <samuelst_pierre@hotmail.com>
2021-08-18 00:48:59 -0400

    Adjusted the way emojis work (In a json now) and changed how the sql requests gets executed and started the blindtest functions

---
+++ functions/blindtest.py
@@ -0,0 +1,8 @@
+async def blindtestChannelChoiceFunc(prompt, emoji):
+    #If the user chose to accept
+    if emoji == prompt.bot.emojisDict["whiteCheckMark"]:
+        
+        return
+    else:
+        return
+
